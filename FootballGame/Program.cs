d52008391672210c3201036cf8a37f2a36b94142
ben Petersen <ben83pete@gmail.com>
2019-02-12 14:22:34 -0500

    Rearrainged

--- FootballGame/Program.cs
+++ FootballGame/Program.cs
@@ -4,66 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FootballGame
-
-    {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Console.WriteLine("FootBall Game - First to 21");
-
-            int Downs = 0;
+namespace FootballGame {
+    class Program {
+        static void Main(string[] args) {
+            //int Downs = 0;
             int DownYards = 0;
-            int TotalYards = 90;
+            int TotalYards = 70;
             int Score = 0;
-            
+
             Random randomNumber = new Random();
-                       
-                        
-            for (int downs = 0; downs < 4; downs++)
-            {
+
+
+            for (int downs = 1; downs < 4; downs++) {
+                //Downs++;
                 int yards = randomNumber.Next(0, 10);
-                Downs++;                               
                 TotalYards += yards;
 
-                if (TotalYards >= 100)
-                {
-                    DownYards = 0;
-                    Downs = 0;
-                    Score += 7;
-                    TotalYards = 90;
-                    Console.WriteLine($"Team A scored {Score}");
-                    continue;
-                }
-                else
-                {
-                    Console.WriteLine($"{Downs + 1} and {(10 - DownYards) - yards} to go.");
-                }
-
-
-                if (Downs < 4)
-                {       // Do nothing           
-                }
-                else Console.WriteLine("TurnOver on Downs");
-                ;
-
-
-                if (DownYards < 10)
-                {
+                if (DownYards < 10) {
                     DownYards += yards;   // ---go back to random #
                 }
-                else
-                {
+                else {
+                    Console.WriteLine($"{downs + 1} and {(10 - DownYards) - yards} to go.");
+                }
+
+                if (downs < 4) {
+                    //Do Nothing
+                }
+                else {
+                    Console.WriteLine("TurnOver on Downs");
+                    break;
+                }
+
+                if (TotalYards >= 100) {
+                    DownYards = 0;
+                    downs = 0;
+                    Score += 7;
+                    TotalYards = 90;
+                    Console.WriteLine($"Fairfield scored {Score}");
+                    break;
+                }
+                else {
                     Console.WriteLine($"First Down and {100 - TotalYards} to goal line.");
                     DownYards = 0;
-                    Downs = 0;
-                    
+                    downs = 0;
                 }
-                               
-            }            
-        }           
-    }       
-}
 
-   
+            }
+        
+        }
+    }
+
+}
